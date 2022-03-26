4b0a520dcfc1af40bea8b0dcbeba8a4dad789d5d
lvyamin <123456aA/>
2022-03-26 21:40:18 +0800

    add:zeus-common更新

---
+++ zeus-common/src/main/java/zeus/config/ZeusClientConfiguration.java
@@ -0,0 +1,17 @@
+package zeus.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lym
+ */
+@Configuration
+public class ZeusClientConfiguration {
+
+
+
+
+
+
+
+}
